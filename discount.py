dcad8e932eab2fdc413c214933966911d94a1cd2
Do Thi My Le <dothimyle431@gmail.com>
2026-06-10 14:46:34 +0700

    Implement loyalty customer discount feature

---
+++ discount.py
@@ -0,0 +1,5 @@
+def discount (order_before):
+    if order_before >= 50000000:
+        return 0.1
+    return 0
+        
---
+++ test_discount.py
@@ -0,0 +1,5 @@
+from discount import discount
+def test_case_1():
+    assert discount(50000000) == 0.1
+def test_case_2():
+    assert discount(30000000) == 0
